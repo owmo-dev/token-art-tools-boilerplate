10e9d45d66b6f134696d4317796bdb291cc4324a
Owen Moore <hello@owenmoore.xyz>
2021-11-29 17:05:18 -0800

    added public folder, for importing media

--- webpack.config.js
+++ webpack.config.js
@@ -41,4 +41,9 @@ module.exports = {
             },
         ],
     },
+    output: {
+        path: path.resolve(__dirname, "./src"),
+        filename: "app.js",
+        publicPath: "/",
+    },
 };
